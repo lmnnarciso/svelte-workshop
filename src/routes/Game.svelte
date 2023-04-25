c5c8f3cb42374a44b5c4db96a88f09e10eba7236
lmnnarciso <lmnnarciso14@gmail.com>
2023-04-25 14:57:41 +0800

    Initial app config

---
+++ src/routes/Game.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Grid from './Grid.svelte';
+</script>
+
+<div class="game">
+	<div class="info" />
+	<div class="grid-container">
+		<Grid />
+	</div>
+	<div class="info" />
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		font-size: min(1em, 0.3rem);
+	}
+
+	.info {
+		width: 80em;
+		height: 10em;
+		background: purple;
+	}
+
+	.grid-container {
+		width: 80em;
+		height: 80em;
+		background: teal;
+	}
+</style>
