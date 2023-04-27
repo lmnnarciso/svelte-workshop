b7bdc67bc1b39521ec63c0cc34acc01194fa3073
lmnnarciso <lmnnarciso14@gmail.com>
2023-04-27 13:07:01 +0800

    Pairs

--- src/routes/Found.svelte
+++ src/routes/Found.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { get_twemoji_url } from './utils';
+	import { receive } from './transitions';
 
 	export let found: string[];
 </script>
 
 <div class="found">
 	{#each found as emoji}
-		<img alt={emoji} src={get_twemoji_url(emoji)} />
+		<div class="pair">
+			<img in:receive={{ key: `${emoji}:a` }} alt={emoji} src={get_twemoji_url(emoji)} />
+			<img in:receive={{ key: `${emoji}:b` }} alt={emoji} src={get_twemoji_url(emoji)} />
+		</div>
 	{/each}
 </div>
 
@@ -16,9 +20,24 @@
 		justify-content: center;
 		align-items: center;
 		gap: 0.5em;
+		height: 100%;
+		filter: drop-shadow(0.2em 0.2em 0.5em rgba(0, 0, 0, 0.2));
 	}
-	img {
+
+	.pair {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		width: 4em;
 		height: 4em;
+		background: white;
+		padding: 1em;
+		border-radius: 50%;
+	}
+
+	img {
+		position: absolute;
+		width: 2em;
+		height: 2em;
 	}
 </style>
